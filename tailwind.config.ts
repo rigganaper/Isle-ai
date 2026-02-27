43f8fcdbfedc97e884a8f47da0434210e643d978
rigganaper <jasonninja2@gmail.com>
2026-02-27 10:33:33 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+Isle-ai
+/next.config.ts
+rigganaper
+rigganaper
+Día 1: Cimientos de Isle AI
+47ef779
+ · 
+13 hours ago
+
+Code
+
+Blame
+8 lines (6 loc) · 156 Bytes
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+};
+
+export default nextConfig;
